a29edfcd34e3d4b925774e8edebae48990406172
hemel <54486516+hemel10242048@users.noreply.github.com>
2021-08-23 23:14:10 +0600

    update (11:15 p.m) 8-23-2021

--- playlist.js
+++ playlist.js
@@ -1,19 +1,20 @@
 import songslist from './songslist.js'
 const Playlist=(()=>{
     const songs=songslist;
-    const cpi=0;
+    const cpi=2;
     const currentsong=new Audio(songs[cpi].url);
     const playlistelements=document.querySelector('.playlist')
     const init=()=>{
         console.log("Hello from spotify!");
         console.log(songs);
+        currentsong.play()
         render();
     };
     const render=()=>{
         let markup='';
-        songs.forEach((song)=>{
+        songs.forEach((song,index)=>{
             markup+=`
-            <li class="playlist_song">
+            <li class="playlist_song ${index==cpi?'playlist_active':''}">
             <span class="song_name">${song.id}.</span>
                 <span class="song_name">${song.title}</span>
                 <span class="song_artist">${song.Artist}</span>
