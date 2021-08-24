0b34326498f6c93824c4d3d94c4be9c51483cf5b
hemel <54486516+hemel10242048@users.noreply.github.com>
2021-08-24 16:32:32 +0600

    Update songslist.js

--- songslist.js
+++ songslist.js
@@ -3,44 +3,51 @@ const songslist=[
         id: '1',
         title: 'Faded',
         url: 'https://usaupload.com/file/4CuQ/Faded.mp3',
-        Artist: 'Alan Walker',
+        artist: 'Alan Walker',
         time: '03:53',
     },
     {
         id: '2',
         title: 'Alone',
         url: 'https://usaupload.com/file/4CuU/Alone.mp3',
-        Artist: 'Alan Walker',
+        artist: 'Alan Walker',
         time: '03:53',
     },
     {
         id: '3',
         title: 'Darkside',
         url: 'https://usaupload.com/file/4CuS/Darkside.mp3',
-        Artist: 'Alan Walker',
+        artist: 'Alan Walker',
         time: '03:53',
     },
     {
         id: '4',
         title: 'The Spectre',
         url: 'https://usaupload.com/file/4CuR/The_Spectre.mp3',
-        Artist: 'Alan Walker',
+        artist: 'Alan Walker',
         time: '03:53',
     },
     {
         id: '5',
         title: 'On My Way',
         url: 'https://usaupload.com/file/4CuT/On_My_Way.mp3',
-        Artist: 'Alan Walker',
+        artist: 'Alan Walker',
         time: '03:53',
     },
     {
         id: '6',
         title: 'Illuminati',
         url: 'https://usaupload.com/file/O5G/illuminati_song__Anonymous.mp3',
-        Artist: 'Anonymous',
+        artist: 'Anonymous',
         time: '03:53',
+    },
+    {
+        id: '7',
+        title: 'Lily',
+        url: 'https://usaupload.com/file/4CX0/Lily.mp3',
+        artist: 'Alan Walker',
+        time: '03:25',
     }
 ];
 
-export default songslist;
+export default songslist
